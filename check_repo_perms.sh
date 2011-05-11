4a680105dbeae4988a1ec2bcbf8988558afc4761
Gordon Maclean <maclean@ucar.edu>
2011-05-11 17:09:27 +0000

    send email for files owned by ads to cjw
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/repo/scripts@8010 da89c675-a4d7-0310-8a15-9e7546503515

--- check_repo_perms.sh
+++ check_repo_perms.sh
@@ -19,7 +19,7 @@ for o in ${offenders[*]}; do
     cat << EOD > $tmpfile2
 A friendly reminder from $0 ...
 
-The following files on $repo are owned by you and don't have
+The following files on $repo are owned by $o and don't have
 group write permission, preventing others from updating the repository.
 
 The rmpbuild and createrepo commands ignore your umask and create files without 
@@ -30,7 +30,8 @@ called fixup_repo.sh which will run createrepo on all repositories in $repo
 and then enable group write permission on all files owned by you.
 
 A working copy of http://svn.eol.ucar.edu/svn/eol/repo/scripts is at
-/net/www/docs/software/rpms/scripts. So you can run the fixup script directly:
+/net/www/docs/software/rpms/scripts. You can run the fixup script directly,
+from the $o login:
     /net/www/docs/software/rpms/scripts/fixup_repo.sh
 
 The scripts repository also includes a bash function called
@@ -40,8 +41,14 @@ to the repository, run createrepo and fix the permissions.
 EOD
 
     awk '$5 == "'$o'"{print $0}' $tmpfile >> $tmpfile2
+    to=$o
+    case $o in
+        ads)
+            to=cjw
+            ;;
+    esac
 
-    env MAILRC=/dev/null smtp=smtp.eol.ucar.edu from=$USER@ucar.edu mailx -s "File(s) on $repo without group write" $o  < $tmpfile2
+    env MAILRC=/dev/null smtp=smtp.eol.ucar.edu from=$USER@ucar.edu mailx -s "File(s) on $repo without group write" $to  < $tmpfile2
     env MAILRC=/dev/null smtp=smtp.eol.ucar.edu from=$USER@ucar.edu mailx -s "File(s) on $repo without group write" $USER  < $tmpfile2
 done
 
