7e213c8f67c7981dc9f8448c93c8b9b4f449b27d
Gary Granger <granger@ucar.edu>
2013-02-14 22:02:37 +0000

    this started out as a change to prevent warning messages from find about the use of execdir with '.' in the PATH, then it became a refactor of the other uses of find
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/repo/scripts@8543 da89c675-a4d7-0310-8a15-9e7546503515

--- fixup_repo.sh
+++ fixup_repo.sh
@@ -5,28 +5,26 @@
 # files have group eol write permission.
 
 repo=/net/www/docs/software/rpms 
+echo=
 
 # createrepo version 0.4.9 (EL5) complains about --checksum sha,
 # "This option is deprecated", but the option is necessary with
 # version 0.9.8 (Fedora 12, etc) to create repodata that is compatible
 # with 0.4.9.
-ver4=false
-createrepo --version | grep -E "^0\.4\.[0-9]+$" > /dev/null && ver4=true
+createrepo="createrepo --checksum sha --update"
+if createrepo --version | grep -Eq "^0\.4\.[0-9]+$" ; then
+    createrepo="createrepo --update"
+fi
 
-for d in `find $repo -name .svn -prune -o -name repodata -type d -print`; do
-    cd ${d%/repodata} || exit 1
-    echo $PWD
-    if $ver4; then
-        createrepo --update .
-    else
-        createrepo --checksum sha --update .
-    fi
-    cd - > /dev/null
+find $repo -name .svn -prune -o -name repodata -type d -print | \
+while read d ; do
+    d=`dirname "$d"`
+    (set -x; $echo $createrepo "$d")
 done
 
-find $repo -user $USER \! -group eol -exec chgrp eol {} \;
-find $repo -user $USER \! -perm -020 -exec chmod g+w {} \;
+find $repo -user $USER \! -group eol -print0 | xargs -0r $echo chgrp eol
+find $repo -user $USER \! -perm -020 -print0 | xargs -0r $echo chmod g+w 
 
 # bug in some versions of createrepo, leaves garbageid directories around
 # https://bugzilla.redhat.com/show_bug.cgi?id=728584
-find $repo -type d -name garbageid -execdir rm -rf {} +
+find $repo -type d -name garbageid -print0 | xargs -0r $echo rm -rf
