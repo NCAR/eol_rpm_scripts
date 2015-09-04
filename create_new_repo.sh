c082d38bf6a4678b1d3510fc0c7f86c277c29f29
Gordon Maclean <maclean@ucar.edu>
2015-09-04 16:12:02 +0000

    old updates
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/repo/scripts@9381 da89c675-a4d7-0310-8a15-9e7546503515

--- create_new_repo.sh
+++ create_new_repo.sh
@@ -7,29 +7,31 @@ if [ $# -lt 1 ]; then
     echo "Usage: $0 subdir"
     echo "Examples:
 $0 fedora/15
-$0 epel/6"
+$0 epel/6
 $0 fedora-signed/15
 $0 epel-signed/6"
     exit 1
 fi
 
-repo=/net/www/docs/software/rpms
+repo=$1
+
+repotop=/net/www/docs/software/rpms
 
 for s in SRPMS i386 x86_64; do
-    echo "creating $repo/$1/$s/repodata"
-    mkdir -p $repo/$1/$s/repodata || exit 1
+    echo "creating $repotop/$repo/$s/repodata"
+    mkdir -p $repotop/$repo/$s/repodata || exit 1
 done
 
 # create symbolic links for epel
-if [[ $1 =~ ^epel/[0-9]+$ ]]; then
+if [[ $repo =~ ^epel/[0-9]+$ ]]; then
     eversion=${1##*/}
-    ln -s $eversion $repo/${1}Client
-    ln -s $eversion $repo/${1}Server
-    ln -s $eversion $repo/${1}Workstation
+    ln -s $eversion $repotop/${1}Client
+    ln -s $eversion $repotop/${1}Server
+    ln -s $eversion $repotop/${1}Workstation
 fi
 
 # Run createrepo
 dir=`dirname $0`
-$dir/fixup_repo.sh
+$dir/fixup_repo.sh $repo
 
 
