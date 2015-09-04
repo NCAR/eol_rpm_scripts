752a4881ff07c943364e0b4681dbc74ae6346803
Gordon Maclean <maclean@ucar.edu>
2015-09-04 16:12:31 +0000

    variable name change
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/repo/scripts@9382 da89c675-a4d7-0310-8a15-9e7546503515

--- fixup_repo.sh
+++ fixup_repo.sh
@@ -4,7 +4,7 @@
 # that have a repodata directory, then makes sure all the user's
 # files have group eol write permission.
 
-repo=/net/www/docs/software/rpms 
+repotop=/net/www/docs/software/rpms 
 echo=
 
 # createrepo version 0.4.9 (EL5) complains about --checksum sha,
@@ -15,7 +15,14 @@ echo=
 crver4=false
 createrepo --version | grep -q -E "^0\.4\.[0-9]+$" > /dev/null && crver4=true
 
-for d in `find $repo -name .svn -prune -o -name repodata -type d -print`; do
+repos=
+if [ $# -gt 0 ]; then
+    repos=($1)
+else
+    repos=($(find $repotop -name .svn -prune -o -name repodata -type d -print))
+fi
+
+for d in ${repos[*]}; do
     cd ${d%/repodata} || exit 1
     echo $PWD
 
@@ -38,9 +45,9 @@ for d in `find $repo -name .svn -prune -o -name repodata -type d -print`; do
     cd - > /dev/null
 done
 
-find $repo -user $USER \! -group eol -print0 | xargs -0r $echo chgrp eol
-find $repo -user $USER \! -perm -020 -print0 | xargs -0r $echo chmod g+w 
+find $repotop -user $USER \! -group eol -print0 | xargs -0r $echo chgrp eol
+find $repotop -user $USER \! -perm -020 -print0 | xargs -0r $echo chmod g+w 
 
 # bug in some versions of createrepo, leaves garbageid directories around
 # https://bugzilla.redhat.com/show_bug.cgi?id=728584
-find $repo -type d -name garbageid -print0 | xargs -0r $echo rm -rf
+find $repotop -type d -name garbageid -print0 | xargs -0r $echo rm -rf
