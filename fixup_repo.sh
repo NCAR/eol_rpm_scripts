85ee08d4fa2c211a2385f92792ca00365eed1e19
Gordon Maclean <maclean@ucar.edu>
2016-04-05 17:40:22 +0000

    Don't run createrepo in this script
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/repo/scripts@9574 da89c675-a4d7-0310-8a15-9e7546503515

--- fixup_repo.sh
+++ fixup_repo.sh
@@ -1,49 +1,10 @@
 #!/bin/sh
 
-# script to run createrepo on all directories in the repository
-# that have a repodata directory, then makes sure all the user's
-# files have group eol write permission.
+# makes sure all the user's files in the repository
+# have group eol write permission.
 
 repotop=/net/www/docs/software/rpms 
 echo=
-
-# createrepo version 0.4.9 (EL5) complains about --checksum sha,
-# "This option is deprecated", but the option is necessary with
-# newer versions of createrepo to create repositories compatible with
-# yum 3.2.*
-
-crver4=false
-createrepo --version | grep -q -E "^0\.4\.[0-9]+$" > /dev/null && crver4=true
-
-repos=
-if [ $# -gt 0 ]; then
-    repos=($1)
-else
-    repos=($(find $repotop -name .svn -prune -o -name repodata -type d -print))
-fi
-
-for d in ${repos[*]}; do
-    cd ${d%/repodata} || exit 1
-    echo $PWD
-
-    if $crver4; then
-        echo "createrepo ."
-        flock . -c "createrepo ." || exit 1
-    else
-        if echo $d | fgrep -q epel/5; then
-            echo "createrepo --checksum sha ."
-            flock . -c "createrepo --checksum sha ." || exit 1
-        elif echo $d | fgrep -q ael; then
-            echo "createrepo --checksum sha ."
-            flock . -c "createrepo --checksum sha ." || exit 1
-        else
-            echo "createrepo ."
-            flock . -c "createrepo ." || exit 1
-        fi
-    fi
-
-    cd - > /dev/null
-done
 
 find $repotop -user $USER \! -group eol -print0 | xargs -0r $echo chgrp eol
 find $repotop -user $USER \! -perm -020 -print0 | xargs -0r $echo chmod g+w 
