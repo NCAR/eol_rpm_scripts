fa03e118ed272e4e9dfa60462b64125d765eb74a
Gordon Maclean <maclean@ucar.edu>
2016-08-08 16:05:50 +0000

    check for .olddata
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/repo/scripts@9650 da89c675-a4d7-0310-8a15-9e7546503515

--- fixup_repo.sh
+++ fixup_repo.sh
@@ -12,3 +12,12 @@ find $repotop -user $USER \! -perm -664 -print0 | xargs -0r $echo chmod ug+w,ugo
 # bug in some versions of createrepo, leaves garbageid directories around
 # https://bugzilla.redhat.com/show_bug.cgi?id=728584
 find $repotop -type d -name garbageid -print0 | xargs -0r $echo rm -rf
+
+# Not sure what circumstance results in .olddata directories
+# laying around. createrepo fails if it finds them.
+oldies=($(find $repotop -name .olddata))
+if [ ${#oldies[*]} -gt 0 ]; then
+    echo "Warning: found ${#oldies[*]} .olddata directories. Deleting..."
+    rm -rf ${oldies[*]}
+fi
+
